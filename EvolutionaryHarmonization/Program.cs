a9bc321003ff42ba011853ac0725b904356f6f28
Jan Mycka <jkj.mycka@gmail.com>
2021-01-06 14:53:41 +0100

    Dodaj pliki projektów.

---
+++ EvolutionaryHarmonization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EvolutionaryHarmonization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
